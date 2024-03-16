b884d88ffa6459e8400bf0469b021b117440f92b
Erimsah Olut <erimsaholut@icloud.com>
2024-03-16 18:04:56 +0300

    main menu and words page scaffold

---
+++ lib/pages/words_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WordsPage extends StatelessWidget {
+  const WordsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
