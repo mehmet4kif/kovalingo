9aa89a1909296275d6a45feff5ba9232436dd346
Erimsah Olut <erimsaholut@icloud.com>
2024-03-17 18:55:18 +0300

    constant files and logo

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+  static Color appBarBlue = const Color(0XFF378CE7);
+  static Color menuButtonBlue = const Color(0XFF67C6E3);
+  static Color backgroundBlue = const Color(0XFFDFF5FF);
+  static Color wordPackageBlue = const Color(0XFF008DDA);
+
+
+
+}
---
+++ lib/pages/words_sub_pages/word_packages_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:kovalingo/constants/colors.dart';
+
+import '../../widgets/words_package_button.dart';
+
+class WordPackagePage extends StatelessWidget {
+  WordPackagePage({super.key});
+
+  List<Widget> wordPackages = [
+    WordPackageButton(
+      text: 'Stanford En Çok Kullanılan 100 kelime',
+      onTap: () {},
+    ),
+    WordPackageButton(
+      text: 'Stanford En Çok Kullanılan 250 kelime',
+      onTap: () {},
+    ),
+    WordPackageButton(
+      text: 'Stanford En Çok Kullanılan 500 kelime',
+      onTap: () {},
+    ),
+    WordPackageButton(
+      text: 'Stanford En Çok Kullanılan 1000 kelime',
+      onTap: () {},
+    ),
+    WordPackageButton(
+      text: 'Tatil için 120 Turist Kelimesi',
+      onTap: () {},
+    ),
+    WordPackageButton(
+      text: 'A1 Seviyesi 200 kelime',
+      onTap: () {},
+    ),
+    WordPackageButton(
+      text: 'Yazılım ile ilgili 80 kelime',
+      onTap: () {},
+    ),
+    WordPackageButton(
+      text: 'Prepositions kelimeleri',
+      onTap: () {},
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.backgroundBlue,
+      appBar: AppBar(
+        title: const Text('Word Package Page'),
+        backgroundColor: CustomColors.appBarBlue,
+      ),
+      body: GridView.count(
+        padding: EdgeInsets.all(16.0),
+        mainAxisSpacing: 16.0,
+        crossAxisSpacing: 16.0,
+        crossAxisCount: 2,
+        children: wordPackages,
+      ),
+    );
+  }
+}
+
+
